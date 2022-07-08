e24dfa661b5a9bd37a96a95e5f3c4628e4196994
sunnyfterrain <sunnyfterrain@gmail.com>
2022-07-08 15:02:21 +0900

    ADD: vue-router setting

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
-  <div id="app"><router-view></router-view></div>
+  <div id="app">
+    <tool-bar></tool-bar>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-export default {};
+import ToolBar from './components/ToolBar.vue';
+export default {
+  components: {
+    ToolBar,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  padding: 0;
+  margin: 0;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,17 @@ import VueRouter from 'vue-router';
 import NewsView from '../views/NewsView.vue';
 import AskView from '../views/AskView.vue';
 import JobsView from '../views/JobsView.vue';
+import UserView from '../views/UserView.vue';
+
 Vue.use(VueRouter);
 
 export const router = new VueRouter({
+  mode: 'history',
   routes: [
+    { path: '', redirect: '/news' },
     { path: '/news', component: NewsView },
     { path: '/ask', component: AskView },
     { path: '/jobs', component: JobsView },
+    { path: '/user', component: UserView },
   ],
 });
