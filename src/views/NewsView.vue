512eabd8bd591942deb47e7f1473511073d2d90a
sunnyfterrain <sunnyfterrain@gmail.com>
2022-07-12 23:56:31 +0900

    ADD: dynamic routing

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -4,7 +4,7 @@
       <a v-bind:href="item.url">{{ item.title }}</a>
       <small>
         {{ item.time_ago }} by
-        <router-link :to="'/user' + item.id">{{ item.user }}</router-link>
+        <router-link :to="`/user/${item.user}`">{{ item.user }}</router-link>
       </small>
     </p>
   </div>
