0e717d8b15993166097b873926667e7413038bee
sunnyfterrain <sunnyfterrain@gmail.com>
2022-07-24 22:13:00 +0900

    Modify: newsview styling

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,20 @@ body {
   margin: 0;
 }
 
+a {
+  color: #34495e;
+  text-decoration: none;
+}
+
+a:hover {
+  color: #42b883;
+  text-decoration: underline;
+}
+
+a.router-link-exact-active {
+  text-decoration: underline;
+}
+
 .fade-enter-active,
 .fade-leave-active {
   transition: opacity 0.5s;
--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,12 +1,28 @@
 <template>
   <div>
-    <p v-for="(item, idx) in this.$store.state.news" :key="idx">
-      <a v-bind:href="item.url">{{ item.title }}</a>
-      <small>
-        {{ item.time_ago }} by
-        <router-link :to="`/user/${item.user}`">{{ item.user }}</router-link>
-      </small>
-    </p>
+    <ul class="news-list">
+      <li v-for="(item, idx) in this.$store.state.news" :key="idx" class="post">
+        <!-- 포인트 영역-->
+        <div class="points">
+          {{ item.points }}
+        </div>
+        <!-- 기타 정보 영역 -->
+        <div>
+          <p class="news-title">
+            <a v-bind:href="item.url">{{ item.title }}</a>
+          </p>
+          <small class="link-text">
+            {{ item.time_ago }} by
+            <router-link :to="`/user/${item.user}`" class="link-text">{{ item.user }}</router-link>
+          </small>
+        </div>
+        <!-- <a v-bind:href="item.url">{{ item.title }}</a>
+        <small>
+          {{ item.time_ago }} by
+          <router-link :to="`/user/${item.user}`">{{ item.user }}</router-link>
+        </small> -->
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -18,4 +34,31 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.news-list {
+  margin: 0;
+  padding: 0;
+}
+.post {
+  display: flex;
+  align-items: center;
+  border: 1px solid #eee;
+}
+
+.points {
+  width: 80px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #42b883;
+}
+
+.news-title {
+  margin: 0;
+}
+
+.link-text {
+  color: #828282;
+}
+</style>
